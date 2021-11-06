ecfaf6494da0da14f131a1749f8d17be13ecb0b6
sayingu <sayingu@gmail.com>
2021-11-06 14:02:17 +0900

    sas

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import { habitsState } from './atom';
 
 export default function Navbar() {
   const [habits, setHabits] = useRecoilState(habitsState);
-  console.log(habits.map());
+  console.log(habits.forEach());
 
   return (
     <nav className="navbar">
